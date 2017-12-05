b8f64946f8330fc7fc7e08988f7f8c9fb03f835a
Hola <hola@andalaosa.cl>
2017-12-05 18:09:02 -0300

    new(files): first release of awesome-devschile-frontend

---
+++ site-frontend/README.md
@@ -0,0 +1,23 @@
+# site-frontend
+
+> frontend for awesome-devschile
+
+get README from [awesome-devschile](https://github.com/devschile/awesome-devschile) and display content in [https://awesome.devschile.cl](https://awesome.devschile.cl)
+
+## Build Setup
+
+``` bash
+# install dependencies
+yarn
+
+# serve with hot reload at localhost:8080
+yarn run dev
+
+# build for production with minification
+yarn run build
+
+# build for production and view the bundle analyzer report
+yarn run build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ site-frontend/src/components/Home.vue
@@ -0,0 +1,107 @@
+<template>
+  <main>
+    <vue-markdown :source="content"></vue-markdown>
+  </main>
+</template>
+
+<script>
+import axios from 'axios'
+import VueMarkdown from 'vue-markdown'
+
+export default {
+  name: 'Home',
+  data () {
+    return {
+      content: '',
+      errors: []
+    }
+  },
+  components: {
+    VueMarkdown
+  },
+  methods: {
+    getDataFromGithub: function () {
+      axios.get('https://api.github.com/repos/devschile/awesome-devschile/readme')
+      .then(response => {
+        this.content = atob(response.data.content)
+      })
+      .catch(error => {
+        this.errors = error
+      })
+    }
+  },
+  created: function () {
+    this.getDataFromGithub()
+  }
+}
+</script>
+
+<style lang="scss">
+html,
+body {
+  height: 100%;
+  box-sizing: border-box; 
+}
+html {
+  overflow-x: hidden;
+}
+body {
+  background: #000;
+  color: #ddd;
+  font-size: 14px;
+}
+body,
+pre,
+code {
+  font-family: 'Inconsolata', monospace;
+}
+h1,
+h2,
+h3,
+pre,
+code {
+  color: #fff;
+  font-weight: normal;
+}
+code {
+  background-color: rgba(255,255,255,.15);
+}
+a {
+  color: #5ce6cd;
+}
+main {
+  padding: 5em 1.5em;
+}
+
+h1 {
+  font-size: 18px;
+}
+h2 {
+}
+h3 {
+  margin-top: 20px;
+}
+h2,
+h3 {
+  font-size: 16px;
+}
+hr {
+  opacity: .4;
+}
+.btn {
+  display: inline-block;
+  padding: .2em .6em;
+  color: #5ce6cd;
+  border: 1px solid #5ce6cd;
+  text-decoration: none;
+  text-transform: uppercase;
+}
+button.btn {
+  margin: 0 .4em;
+  background-color: transparent;
+  cursor: pointer;
+  color: #2980B9;
+  border-color: #2980B9;
+}
+
+</style>
